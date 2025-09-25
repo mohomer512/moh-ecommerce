aad47c56c5bf71f3e0024fafea628f88f1b9aa92
mohomer512 <drprogrammer512@hotmail.com>
2025-09-26 01:58:52 +0300

    shadcn componenets added

---
+++ global.d.ts
@@ -0,0 +1,33 @@
+declare module "*.css";
+declare module "*.scss";
+declare module "*.sass";
+
+// Image files
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+declare module "*.jpeg" {
+  const value: string;
+  export default value;
+}
+declare module "*.gif" {
+  const value: string;
+  export default value;
+}
+
+// SVG files
+declare module "*.svg" {
+  import * as React from "react";
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  const src: string;
+  export default src;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-bold">Welcome to the Home Page</h1>
-    </main>
-  );
+    <div className="flex flex-col items-start gap-6 p-6 max-w-md mx-auto">
+      <Button>Im a button</Button>
 
+      <Input placeholder="I'm an input" />
+
+      <Progress value={50} className="w-full" />
+
+      <Textarea placeholder="I'm a text area" />
+
+      <Checkbox> checkbox </Checkbox>
+    </div>
+  );
 }
